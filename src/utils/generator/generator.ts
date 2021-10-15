54aae2a566beb296941a52e68044d3a03f246abd
franlol <fraaaaan@gmail.com>
2021-10-16 01:43:18 +0200

    feat: add jsdoc to utils

--- src/utils/generator/generator.ts
+++ src/utils/generator/generator.ts
@@ -1,5 +1,16 @@
 import { ECharactersList, TgenerateCharactersListArgs } from "./generator.types";
 
+/**
+ * Return a string with all the characters needed to generate a password.
+ * @param {boolean} flags.upperCase Password will contain uperrcase characters.
+ * @param {boolean} flags.lowerCase Password will contain lowerCase characters.
+ * @param {boolean} flags.numbers Password will contain numeric characters.
+ * @param {boolean} flags.simbols Password will contain simbols.
+ * @param {boolean} flags.specialChars Password will contain special characters.
+ * 
+ * @returns {string} Characters string
+ * @example "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*"
+ */
 export const generateCharactersList = (flags: TgenerateCharactersListArgs): string => {
   const {
     upperCase,
@@ -20,11 +31,18 @@ export const generateCharactersList = (flags: TgenerateCharactersListArgs): stri
   return charactersList.trim();
 }
 
-export const generatePassword = (characterList: string, length: number) => {
-  const maxLength = length <= 100 ? length : 100;
+/**
+ * Returns a password, randomly matching the characterList param string .
+ * @param characterList String with characters to use when generating the password.
+ * @param length Desired length of the password.
+ * @returns {String} The password.
+ * @example "^0M8Wjsm*i5Dcj9!2#g8ouMC"
+ */
+export const generatePassword = (characterList: string, length: number = 16): string => {
+  const maxLength: number = length <= 100 ? length : 100;
   const passwordLengthEmptyArray: Array<null> = Array(maxLength).fill(null);
 
-  const password = passwordLengthEmptyArray.reduce((prev) => {
+  const password: string = passwordLengthEmptyArray.reduce((prev) => {
     return prev.concat(characterList.charAt(Math.floor(Math.random() * characterList.length)));
   }, '');
 
